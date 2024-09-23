e263c2fa74a24e493d74cba9def9d827aa2e0674
gulan4ik <99678468+gulan4ik@users.noreply.github.com>
2024-09-23 20:07:22 +0300

    PractRabota1.1.cpp

--- PractRabota1.1.cpp
+++ PractRabota1.1.cpp
@@ -22,7 +22,71 @@ void Int()
 
     {
 
-        cout << ((number >> i) & 1);
+        cout << ((number >> i) & 1); // бинарный сдвиг вправо и умножение на 1 для получения побитового представления числа
+
+        if (i == sizeof(int) * 8 - 1)
+
+            cout << ' ';
+
+    }
+
+    cout << endl;
+}
+void Reverse()
+{
+    float checkIsNotInt;
+    int number;
+    bool isEven = false;
+    bool isNegative;
+    cout << "Введите число" << "\n";
+    cin >> checkIsNotInt;
+    while (true)
+    {
+        number = checkIsNotInt;
+        if (number != checkIsNotInt)
+        {
+            cout << "Ошибка, введите целое число" << "\n";
+            cin >> checkIsNotInt;
+        }
+        else
+            break;
+    }
+    for (int i = sizeof(int) * 8 - 1; i >= 0; --i)
+
+    {
+        if (i == sizeof(int) * 8 - 1)
+        {
+            cout << ((number >> i) & 1);
+            isNegative = (number >> i) & 1;
+            i--;
+        }
+        if (isNegative)
+        {
+            if (isEven)
+            {
+                cout << (((number >> i) & 1 - 1) & 0); // замена на 0
+                isEven = false;
+            }
+            else
+            {
+                cout << ((number >> i) & 1); // бинарный сдвиг вправо и умножение на 1 для получения побитового представления числа
+                isEven = true;
+            }
+        }
+
+        else
+        {
+            if (isEven)
+            {
+                cout << ((number >> i) & 1); // бинарный сдвиг вправо и умножение на 1 для получения побитового представления числа
+                isEven = false;
+            }
+            else
+            {
+                cout << (((number >> i) & 1) | 1); // замена на 1
+                isEven = true;
+            }
+        }
 
         if (i == sizeof(int) * 8 - 1)
 
@@ -39,32 +103,14 @@ void Float()
         float number;
         int num;
     };
-    int checkIsNotFloat;
     cout << "Введите число" << "\n";
     cin >> number;
-    while (true)
-    {
-        checkIsNotFloat = number;
-        if (number == checkIsNotFloat)
-        {
-            cout << "Ошибка, введите десятичное число" << "\n";
-            cin >> number;
-        }
-        else
-            break;
-    }
     for (int i = sizeof(int) * 8 - 1; i >= 0; i--)
 
     {
 
         cout << ((num >> i) & 1);
 
-        if (i == sizeof(int) * 8 - 1)
-
-            cout << ' ';
-        if (i == sizeof(int) * 8 - 1.2)
-
-            cout << ' ';
         if ((i == sizeof(unsigned long int) * 8 - 1) || (i == sizeof(unsigned long int) * 8 - 9))
 
             cout << ' ';
@@ -81,20 +127,8 @@ void Double()
         double number; 
         unsigned long int doubArray[2]; 
     };
-    int checkIsNotDouble;
     cout << "Введите число" << "\n";
     cin >> number;
-    while (true)
-    {
-        checkIsNotDouble = number;
-        if (number == checkIsNotDouble)
-        {
-            cout << "Ошибка, введите десятичное число" << "\n";
-            cin >> number;
-        }
-        else
-            break;
-    }
     for (int i = 1; i >= 0; i--)
     {
         for (int j = sizeof(unsigned long int) * 8 - 1; j >= 0; j--)
@@ -113,29 +147,34 @@ int main()
     cout << "int: " << sizeof(int) << "\nshort int: " << sizeof(short int) << "\nlong int: " << sizeof(long int) << "\nfloat: " << sizeof(float) << "\ndouble: "
         << sizeof(double) << "\nlong double: " << sizeof(long double) << "\nchar: " << sizeof(char) << "\nbool: " << sizeof(bool) << "\n";
     int typeOfOperation;
-    cout << "Выберите тип данных:" << "\n" << "1. Integer" << "\n" << "2. Float" << "\n" << "3. Double" << "\n";
+    cout << "\nВыберите тип данных:" << "\n" << "1. Integer" << "\n" << "2. Float" << "\n" << "3. Double" << "\n" << "4. ИДЗ" "\n";
     cin >> typeOfOperation;
     while (true)
     {
-        if (typeOfOperation == 1)
+        switch (typeOfOperation)
+        {
+        case 1:
         {
             Int();
             break;
         }
-        else if (typeOfOperation == 2)
+        case 2:
         {
             Float();
             break;
         }
-        else if (typeOfOperation == 3)
+        case 3:
         {
             Double();
             break;
         }
-        else
+        case 4:
         {
-            cout << "Ошибка, введите 1, 2 или 3." << "\n";
-            cin >> typeOfOperation;
+            Reverse();
+            break;
         }
+        }
+        cout << "\nВыберите тип данных:" << "\n" << "1. Integer" << "\n" << "2. Float" << "\n" << "3. Double" << "\n" << "4. ИДЗ" "\n";
+        cin >> typeOfOperation;
     }
 }
